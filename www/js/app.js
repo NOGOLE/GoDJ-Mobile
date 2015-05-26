e8c4581e95d6999d8c9f97a0db3a88f5f2e4618f
Jyrone Parker <jyrone.parker@gmail.com>
2015-05-26 00:49:47 -0700

    made webapp changes to mobile

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,75 @@
-var app = angular.module('godj',['ngResource','ngRoute']);
+var app = angular.module('godj',['ngResource','ngRoute'])
 
 //REST
-app.factory("Song", function($resource) {
-	return $resource('http://www.godj.nogole.com/api/v1/songs/:id');
+
+//Song Factory ---------------------
+.factory("Song", function($http) {
+	return {
+	get: function() {
+    var songs = [];
+    $http.get('/api/v1/songs').success(function(data){
+
+
+      songs = data;
+  });
+  return songs;
+},
+
+save: function(songObject) {
+        var request = $.post('/api/v1/songs',songObject, function(data) {
+
+return data;
 });
-app.factory("Mood", function($resource) {
-	return $resource('http://www.godj.nogole.com/api/v1/moods/:id');
+	},
+	destroy: function(id) {
+	return $http.delete('/api/v1/songs/' + id);
+}
+}
+
+})
+// Mood Factory -----------------------------------------------------
+.factory("Mood", function($http) {
+	return {
+	get: function() {
+	return $http.get('/api/v1/moods');
+	},
+
+	save: function(moodObject) {
+	//return $.post('/api/v1/moods',data);
+	var request = $.post('/api/v1/moods',moodObject, function(data) {
+
+return data;
+});
+
+	},
+	destroy: function(id) {
+	return $http.delete('/api/v1/moods/' + id);
+}
+}
+
+})
+// Shoutout Factory -----------------------------------------------------
+.factory("Shoutout", function($http) {
+	return {
+	get: function() {
+	return $http.get('/api/v1/shoutouts');
+	},
+
+	save: function(shoutoutObject) {
+	//return $.post('/api/v1/moods',data);
+	var request = $.post('/api/v1/shoutouts',shoutoutObject, function(data) {
+    console.log(data);
+
+
+return data;
+});
+
+	},
+	destroy: function(id) {
+	return $http.delete('/api/v1/shoutouts/' + id);
+}
+}
+
 });
 
 //
@@ -23,72 +87,87 @@ app.config([
 
 app.controller(
 "requestController",
-function($scope,Mood, Song) {
+function($scope,Mood, Song,Shoutout) {
 $scope.song_requestor_name="";
 $scope.song_title="";
 $scope.song_artist="";
-$scope.song_dj_id="";
 $scope.mood_requestor_name="";
 $scope.mood_title="";
-$scope.mood_dj_id="";
+$scope.dj_id="";
 $scope.lat=0.0;
 $scope.long=0.0;
+$scope.songRequests =[];
+$scope.twitterUrl = 'https://twitter.com/intent/tweet?button_hashtag=NOGOLEGoDJ&text=';
+$scope.moodUrl='I just sent a ' + $scope.mood_title + 'mood request to DJ' + $scope.dj_id +'http://goo.gl/V31Ewl';
+$scope.songUrl='I just requested ' + $scope.song_title + ' by '+$scope.song_artist + ' to DJ' + $scope.dj_id +'http://goo.gl/V31Ewl';
 
+$scope.url = 'ws://godj.nogole.com:8080';
+$scope.addSong = function(song){
+  $scope.$apply(function(){
 
-
-//submit mood request
-$scope.submitMood = function() {
-
-var moodObject = {requestor_name:$scope.mood_requestor_name,
-title:$scope.mood_title,
-dj_id:$scope.mood_dj_id,
-lat:$scope.lat,
-long:$scope.long
-};
-
-var request = Mood.save(moodObject, function() {
-	alert("Request Sent!");
-});
-$scope.mood_title ="";
-return request;
+    $scope.songRequests.push(song);
+  });
 
 };
-
-$scope.getGeo = function()
-{
-	if(navigator.geolocation){
-navigator.geolocation.getCurrentPosition(function(pos) {
-	$scope.lat = pos.coords.latitude;
-	$scope.long = pos.coords.longitude;
-});
+//init function
+$scope.init = function() {
+if(navigator.geolocation){
+navigator.geolocation.getCurrentPosition(showPosition);
 }
 else{
 alert("Geolocation is not supported by this browser.");
 }
+
+var larapush = new Larapush($scope.url);
+
+larapush.watch('demo').on('generic.event', function(msgEvent) {
+
+  $scope.addSong(msgEvent.message);
+  console.log($scope.songRequests);
+});
 };
 
+function showPosition(pos){
+$scope.lat = pos.coords.latitude;
+$scope.long = pos.coords.longitude;
+}
 
+$scope.submitShoutout = function() {
 
+  var shoutoutObject = {message:$scope.shoutout_message,dj_id:sessionStorage.dj};
+  console.log(shoutoutObject);
+var request = Shoutout.save(shoutoutObject);
+$scope.show=true;
+};
 
+//submit mood request
+$scope.submitMood = function() {
+var moodObject = {requestor_name:$scope.mood_requestor_name,
+title:$scope.mood_title,
+dj_id:$scope.dj_id,
+lat:$scope.lat,
+long:$scope.long
+};
+sessionStorage.dj = $scope.dj_id;
+var request = Mood.save(moodObject);
+$scope.mood_title ="";
+return request;
+
+}
 //submit song request
 $scope.submitSong = function() {
-
-
 //create JSON object to insert into post
 var songObject = {requestor_name:$scope.song_requestor_name,
 title:$scope.song_title,
 artist:$scope.song_artist,
-dj_id:$scope.song_dj_id,
+dj_id:$scope.dj_id,
 lat:$scope.lat,
 long:$scope.long };
-
-var request = Song.save(songObject, function() {
-	alert("Request Sent!");
-});
+sessionStorage.dj = $scope.dj_id;
+var request = Song.save(songObject);
 $scope.song_title ="";
 $scope.song_artist ="";
 
 return request;
 }
-$scope.getGeo();
 });
